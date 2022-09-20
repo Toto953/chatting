df8018edf68f12a7100592a9a325dfa357843136
Thomas Tofil <tofil.tomek@gmail.com>
2022-09-20 10:56:14 +0200

    Chat

---
+++ client.py
@@ -0,0 +1,57 @@
+from email import message
+import socket
+import threading
+
+HOST, PORT = "localhost", 8989
+FORMAT = "UTF-8"
+
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket.connect((HOST, PORT))
+
+def sending():
+    running = True
+    while running:
+
+        try:
+            message_input = input("> ").encode(FORMAT)
+        except EOFError:
+            print("Error: leaving")
+            running = False
+            break
+
+        try:
+            socket.send(message_input)
+        except OSError:
+            print("Error: SERVER OFF, message not sent...")
+            running = False
+            break
+
+    socket.close()
+
+
+def recving():
+    running = True
+    s_data = ""
+    while running:
+
+        try:
+            s_data = socket.recv(500).decode(FORMAT)
+        except ConnectionAbortedError:
+            running = False
+            print("Error: leaving")
+            break
+        except ConnectionResetError:
+            print("Error: SERVER OFF, battery is low...")
+            break
+
+        if s_data == "quit":
+            running = False
+            print("Error: SERVER OFF, is shutdowned")
+            break
+
+        print('\n',s_data)
+
+    socket.close()
+
+threading.Thread(target=sending).start()
+threading.Thread(target=recving).start()
---
+++ server.py
@@ -0,0 +1,82 @@
+from http import client, server
+import socket
+import threading
+
+HOST, PORT = "localhost", 8989
+FORMAT = "UTF-8"
+
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket.bind((HOST, PORT))
+socket.listen()
+print("> Server ON <")
+
+global clients
+server_on = True
+stop_thread = False
+clients = []
+
+def handling(s_client, h_client):
+    running = True
+    while running:
+
+        try:
+            c_data = s_client.recv(500).decode(FORMAT)
+        except ConnectionResetError:
+            running = False
+            break
+        except ConnectionAbortedError:
+            running = False
+            break
+
+        print(c_data)
+        for i in clients:
+            if i[0] == s_client:
+                if i[-1] == True:
+                    s_client.send("quit".encode(FORMAT))
+                    running = False
+
+    s_client.close()
+    for i in clients:
+        if i[0] == s_client:
+            i[0].close()
+
+def admin():
+    global clients
+    running = True
+    while running:
+        print("""
+            0: shutdown the server
+            1: Number of clients connected
+            2: remove a client
+        """)
+        user_input = input("> ")
+
+        if user_input == '0':
+            for i in clients:
+                i[-1] = True
+            running = False
+
+        elif user_input == '1':
+            print(f"{len(clients)} of clients in server")
+
+    socket.close()
+
+
+threading.Thread(target=admin).start()
+
+
+while server_on:
+
+    try:
+        s_client, h_client = socket.accept()
+    except OSError:
+        for i in clients:
+            i[0].close()
+        print("Server shutdowning")
+        server_on = False
+        break
+
+    clients.append([s_client, h_client, stop_thread])
+    threading.Thread(target=handling, args=(s_client, h_client)).start()
+
+print("Server is OFF.")
